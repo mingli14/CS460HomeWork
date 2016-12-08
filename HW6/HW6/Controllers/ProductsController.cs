c009d3eaa803472cf95ac99688f2fedd6bbf7a2c
mingli14 <mli14@wou.edu>
2016-12-07 16:19:14 -0800

    Add the HW6

---
+++ HW6/HW6/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW6.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public int PageCount { get; set; } = 9;
+
+        // GET: Products
+        public ActionResult Category(int id, int page = 1)
+        {
+            using (AdventureWorks2014Context db = new AdventureWorks2014Context())
+            {
+                ProductSubcategory psc = db.ProductSubcategories.Find(id);
+                IEnumerable<Product> products = psc.Products.OrderBy(p => p.Name).Skip((page - 1) * PageCount).Take(PageCount);
+                ViewBag.Title = psc.Name;
+                ViewBag.ID = id;
+                ViewBag.Page = page;
+                ViewBag.PageMax = Math.Ceiling(psc.Products.Count() / (double)PageCount);
+                return View(products);
+            }
+        }
+
+        public ActionResult Details(int? id)
+        {
+            return View();
+        }
+    }
+}
